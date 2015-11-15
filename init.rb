8c40c48428c7fa38b96e2b2312b4d6939bc20906
hhokawa777 <hhokawa777@gmail.com>
2015-11-15 12:41:19 +0900

    change author to group

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@
 # assign_current_user Redmine plugin.
 #
 # Authors:
-# - hhokawa777@gmail.com
+# - https://github.com/team888
 #
 # Terms of use:
 # - GNU GENERAL PUBLIC LICENSE Version 2
@@ -12,11 +12,11 @@ require "assign_current_user/hooks.rb"
 
 Redmine::Plugin.register :assign_current_user do
   name 'Assign current user plugin'
-  author 'hhokawa777@gmail.com'
+  author 'team888'
   description 'Assign current login user to specified custom field on new Issue ticket.'
-  version '0.1.0'
-  url 'https://github.com/hhokawa777/redmine-assign_current_user-plugin'
-  #author_url 'http://example.com/about'
+  version '0.2.0'
+  url 'https://github.com/team888/redmine-assign_current_user-plugin'
+  #author_url 'https://github.com/team888'
   
   settings :default => {'empty' => true}, :partial => 'settings/assign_current_user_settings'
 end
